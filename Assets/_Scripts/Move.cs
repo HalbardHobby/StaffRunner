3ac7c256096922fb6f6fd59e136bc21df49e1303
Alejandro Espinosa <byakushi93@gmail.com>
2015-02-24 12:27:16 -0500

    commit general
    commit general con lo que se lleva de la aplicación, se removió el
    sintetizador MIDI tempotralmente con el fin de optimizar y retirar bugs.

---
+++ Assets/_Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody r = target.GetComponent<Rigidbody>();
+		r.velocity = new Vector3 (-5, 0, 0);
+		Destroy (target, 15f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
