465b0eaa4ae4b40b93c07bee5b553caff6b0e03e
Alejandro Espinosa <byakushi93@gmail.com>
2015-03-01 20:43:00 -0500

    Fixes & Términos
    Se arregló la transición de acordes y se agregó la terminología
    necesaria.
    
    Correcciones
    
    Eliminados scripts innecesarios. Creadas transisiones de
    estado.

---
+++ Assets/_Scripts/ChordNames.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ChordNames {
+
+	//nombre de los acrodes mayores
+	public static string I   = "I";
+	public static string II  = "II";
+	public static string III = "III";
+	public static string IV  = "IV";
+	public static string V   = "V";
+	public static string VI  = "VI";
+	public static string VII = "VII";
+
+	//nombre de los acordes menores
+	public static string i   = "i";
+	public static string ii  = "ii";
+	public static string iii = "iii";
+	public static string iv  = "iv";
+	public static string v   = "v";
+	public static string vi  = "vi";
+	public static string vii = "vii";
+
+	//nombre de los acordes disminuidos
+	public static string iDim   = "i°";
+	public static string iiDim  = "ii°";
+	public static string iiiDim = "iii°";
+	public static string ivDim  = "iv°";
+	public static string vDim   = "v°";
+	public static string viDim  = "vi°";
+	public static string viiDim = "vii°";
+
+	//distribucionde tonos de los acordes
+	public static int[] mayor = new int[3]{0,4,7};
+	public static int[] minor = new int[3]{0,3,7};
+	public static int[] dim   = new int[3]{0,3,6};
+	public static int[] aug   = new int[3]{0,7,8};
+
+	//semitonos en una escala
+	public static int semitones = 12;
+	public static int middleC = 60;
+
+	//definicion de semitonos de una escala
+	public static int[] Mayor = new int[8]{0,2,4,5,7,9,11,12};
+	public static int[] Minor = new int[8]{0,2,3,5,7,8,10,12};
+}
--- Assets/_Scripts/notesManager.cs
+++ Assets/_Scripts/notesManager.cs
@@ -4,33 +4,52 @@ using UnityEngine.UI;
 
 public class NotesManager : MonoBehaviour {
 	
-	public GameObject[] targets;                // The enemy prefab to be spawned.
-	public float spawnTime = 2f;             // How long between each spawn.
+	public GameObject[] targets;            // The enemy prefab to be spawned.
+	public float spawnTime = 2f;            // How long between each spawn.
+	public float chordTime = 5f;
 
 	public HealthManager salud;				//Vida del jugador
 	public Text Chord;						//Acorde que se esta generando actualmente
 	public Text Scale;						//Escala que se esta utilizando para generar acordes.
 
-	private Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	private Transform[] spawnPoints;        // An array of the spawn points this enemy can spawn from.
+	private Animator progresion;			// Maquina de estados que colabora en la generacion de progresiones.
+	public bool newChord;					// 
 		
 	void Start (){
 		spawnPoints = GetComponentsInChildren<Transform> ();
+		progresion = GetComponent<Animator> ();
+
+		Scale.text="CM";//TODO seleccion y generacion arbitraria de escalas
 
 		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
 		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		InvokeRepeating ("ChangeChord", 0, chordTime);
 	}
 
 	void Update(){
 		//TODO construir logica de generacion
 	}
-	
+
+	void ChangeChord(){
+		AnimatorStateInfo actual = progresion.GetCurrentAnimatorStateInfo (0);
+
+		int top = 3;
+		if (actual.IsName("I"))
+			top = 8;
+		float t = Random.Range (0f, 1f);
+
+		int tr;
+		if(t>0.8f)
+			tr = Random.Range (-1, 0);
+		else
+			tr = Random.Range (1, top);
+
+		progresion.SetInteger ("ChordSel", tr);
+		progresion.SetTrigger ("Transition");
+	}
+
 	void Spawn (){
-		// If the player has no health left...
-		/*if(playerHealth.currentHealth <= 0f)
-		{
-			// ... exit the function.
-			return;
-		}*/
 		
 		// Find a random index between zero and one less than the number of spawn points.
 		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
@@ -39,5 +58,6 @@ public class NotesManager : MonoBehaviour {
 
 		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
 		Instantiate (targets[targetIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		//TODO asignar la nota
 	}
 }
