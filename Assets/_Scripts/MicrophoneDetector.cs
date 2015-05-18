2f42a09705abf549e4c071ff788043cac959db14
HalbardHobby <alejo_sp@outlook.com>
2015-05-18 14:12:40 -0500

    Terminada función de detección de frecuencia

--- Assets/_Scripts/MicrophoneDetector.cs
+++ Assets/_Scripts/MicrophoneDetector.cs
@@ -6,38 +6,43 @@ public class MicrophoneDetector : MonoBehaviour {
 
 	AudioSource audio;
 
+	public float threshold = 0.15f;
+	public int sampleSize = 8192;
+
 	// Use this for initialization
 	void Start () {
 		// Anhade un audio source para almacenar la entrada
-		audio = gameObject.AddComponent<AudioSource> () as AudioSource;
+		audio = gameObject.GetComponent<AudioSource>();
 		// Inicia la grabacion de audio en el dispositivo por defecto
 		audio.clip = Microphone.Start (null, true, 1, AudioSettings.outputSampleRate);
-
+		audio.loop = true;
 		// Evita reproduccion hasta que haya samples suficientes
 		while (!(Microphone.GetPosition(null)>0));
 
+		Debug.Log (AudioSettings.outputSampleRate);
 		audio.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (getAverageVolume() >= threshold)
+			Debug.Log (GetPrincipalFrecuency());
 	}
 
 	float getAverageVolume(){
-		float[] data = new float[1024];
+		float[] data = new float[sampleSize];
 		float amplitude = 0;
 		audio.GetOutputData (data, 0);
 
 		foreach (float s in data)
 			amplitude += Mathf.Abs (s);
 
-		return amplitude / 1024;
+		return amplitude / sampleSize;
 	}
 
 	float GetPrincipalFrecuency(){
 		float freq = 0.0f;
-		float[] data = new float[8192];
+		float[] data = new float[sampleSize];
 		// se hace fourier para poder encontrar las frecuencias
 		audio.GetSpectrumData (data, 0, FFTWindow.BlackmanHarris);
 
@@ -50,7 +55,7 @@ public class MicrophoneDetector : MonoBehaviour {
 				max = data[j];
 		}
 
-		freq = i * AudioSettings.outputSampleRate / 8192;
+		freq = i * AudioSettings.outputSampleRate / (float)sampleSize;
 		return freq;
 	}
 }
