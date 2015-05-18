063104b77367d04568c328cbf222ecf1da781e89
HalbardHobby <alejo_sp@outlook.com>
2015-05-17 21:08:51 -0500

    creación escena para entrenamiento ritmico

--- Assets/_Scripts/Cleaner.cs
+++ Assets/_Scripts/Cleaner.cs
@@ -4,14 +4,7 @@ using System.Collections;
 //Este objeto se encargara de detectar los objetos salidos del campo de juego y procesarlos debidamente
 public class Cleaner : MonoBehaviour {
 
-	private BoxCollider2D collide;
-
 	public HealthManager health;
-
-	// Use this for initialization
-	void Awake() {
-		collide = GetComponent<BoxCollider2D> ();
-	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		Debug.Log (other.gameObject.tag);
---
+++ Assets/_Scripts/RhythmSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhythmSpawn : MonoBehaviour {
+
+	public int BPM = 60;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
