9ac781b3a745cd99b4eeb6221116d21ebc2ceb4c
HalbardHobby <alejo_sp@outlook.com>
2015-05-19 00:01:20 -0500

    merge

---
+++ Assets/_Scripts/RhythmScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhythmScore : MonoBehaviour {
+
+	ShakeDetector shake;
+	BoxCollider2D [] colliders;
+
+
+	// Use this for initialization
+	void Start () {
+		shake = GetComponent<ShakeDetector> ();
+
+		colliders = GetComponentsInChildren<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
